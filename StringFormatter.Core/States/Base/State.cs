419c9a8f0a1f60ca6ac9b587bfca3ac205ea926a
alexb <alexbondar03@gmail.com>
2022-12-19 22:18:13 +0300

    add base state

---
+++ StringFormatter.Core/States/Base/State.cs
@@ -0,0 +1,6 @@
+﻿namespace StringFormatter.Core.States.Base;
+
+internal abstract class State
+{
+    public abstract State GetNext(StateMachineContext context, char token);
+}
---
+++ StringFormatter.Core/States/Base/StateMachineContext.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace StringFormatter.Core.States.Base;
+
+internal class StateMachineContext
+{
+    private readonly IMemberReader _memberReader;
+    private readonly StringBuilder _stringBuilder = new();
+
+    public string Template { get; }
+    public object Target { get; }
+    public int Index { get; private set; }
+
+    public StateMachineContext(IMemberReader memberReader, string template, object target)
+    {
+        _memberReader = memberReader;
+        Template = template;
+        Target = target;
+        Index = 0;
+    }
+
+    public string Result => _stringBuilder.ToString();
+
+    public void Append(string value) => _stringBuilder.Append(value);
+
+    public void Append(char value) => _stringBuilder.Append(value);
+
+    public bool TryGetObjectValue(InterpolationContext context, out string value)
+    {
+        return _memberReader.TryGetObjectValue(Target, context, out value);
+    }
+
+    public void MoveNext() => Index++;
+}
