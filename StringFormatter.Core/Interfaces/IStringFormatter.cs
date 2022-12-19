1c3cc5742668c106b26f790dc465d8b4a567fa35
alexb <alexbondar03@gmail.com>
2022-12-19 19:37:36 +0300

    Add project files.

---
+++ StringFormatter.Core/Interfaces/IStringFormatter.cs
@@ -0,0 +1,6 @@
+﻿namespace StringFormatter.Core.Interfaces;
+
+public interface IStringFormatter
+{
+    string Format(string template, object target);
+}
---
+++ StringFormatter.Core/StringFormatter.cs
@@ -0,0 +1,11 @@
+﻿using StringFormatter.Core.Interfaces;
+
+namespace StringFormatter.Core;
+
+public class StringFormatter : IStringFormatter 
+{
+    public string Format(string template, object target)
+    {
+        throw new NotImplementedException();
+    }
+}
